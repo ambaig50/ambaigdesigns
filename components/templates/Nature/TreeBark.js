cca042de889d8a0da05f1f45ad4b73cd68b09550
ambaig50 <ambaig50@gmail.com>
2026-06-09 06:43:48 +0500

    Add TreeBark component for nature template

---
+++ components/templates/Nature/TreeBark.js
@@ -0,0 +1,9 @@
+export default function TreeBark({ title, description }) {
+  return (
+    <div style={{ background: "#4e342e", width: "600px", height: "900px", padding: "30px", color: "#efebe9" }}>
+      <h1 style={{ fontSize: "30px", fontWeight: "bold" }}>{title}</h1>
+      <p style={{ fontSize: "18px", marginTop: "20px" }}>{description}</p>
+      <div style={{ marginTop: "40px", fontSize: "60px" }}>🌳</div>
+    </div>
+  );
+}
