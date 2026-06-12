5b0f5b51184f3be27c040b1836f7d3d30195d625
ambaig50 <ambaig50@gmail.com>
2026-06-12 20:42:06 +0500

    Add Pinterest OAuth flow to connectPinterest API
    Implemented Pinterest OAuth flow and redirect.

--- pages/api/connectPinterest.js
+++ pages/api/connectPinterest.js
@@ -1,5 +1,9 @@
 // pages/api/connectPinterest.js
 export default async function handler(req, res) {
-  // TODO: Implement Pinterest OAuth flow
-  res.status(200).json({ success: true, platform: "Pinterest" });
+  const clientId = process.env.PINTEREST_CLIENT_ID;
+  const redirectUri = "http://localhost:3000/api/pinterestCallback";
+
+  const authUrl = `https://www.pinterest.com/v18.0/dialog/oauth?client_id=${clientId}&redirect_uri=${redirectUri}&scope=pages_manage_posts,pages_read_engagement`;
+
+  res.redirect(authUrl);
 }
