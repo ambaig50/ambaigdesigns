c52760aeecc75a0234faec8f09db9df51393178b
ambaig50 <ambaig50@gmail.com>
2026-06-12 20:38:45 +0500

    Add Instagram OAuth flow to connectInstagram API
    Implemented Instagram OAuth flow by redirecting to the authentication URL.

--- pages/api/connectInstagram.js
+++ pages/api/connectInstagram.js
@@ -1,5 +1,9 @@
 // pages/api/connectInstagram.js
 export default async function handler(req, res) {
-  // TODO: Implement Instagram OAuth flow
-  res.status(200).json({ success: true, platform: "Instagram" });
+  const clientId = process.env.INSTAGRAM_CLIENT_ID;
+  const redirectUri = "http://localhost:3000/api/instagramCallback";
+
+  const authUrl = `https://www.instagram.com/v18.0/dialog/oauth?client_id=${clientId}&redirect_uri=${redirectUri}&scope=pages_manage_posts,pages_read_engagement`;
+
+  res.redirect(authUrl);
 }
