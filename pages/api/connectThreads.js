4009c26dbbf01fe5f63456f1cf229398934ac9a8
ambaig50 <ambaig50@gmail.com>
2026-06-12 20:45:39 +0500

    Refactor connectThreads API handler
    Removed TODO comment and adjusted response structure.

--- pages/api/connectThreads.js
+++ pages/api/connectThreads.js
@@ -1,10 +1,5 @@
 // pages/api/connectThreads.js
 export default async function handler(req, res) {
-  // TODO: Implement Threads OAuth flow
-  res.status(200).json({ success: true, platform: "Threads" });
-}
-// pages/api/connectThreads.js
-export default async function handler(req, res) {
   const clientId = process.env.THREADS_CLIENT_ID;
   const redirectUri = "http://localhost:3000/api/threadsCallback";
 
