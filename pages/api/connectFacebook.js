b0e46a376156f125fcb5e62d2a7ee049bf07b1e1
ambaig50 <ambaig50@gmail.com>
2026-06-12 20:34:41 +0500

    Add Facebook OAuth implementation
    Implement Facebook OAuth flow and redirect to auth URL.

--- pages/api/connectFacebook.js
+++ pages/api/connectFacebook.js
@@ -1,5 +1,9 @@
 // pages/api/connectFacebook.js
 export default async function handler(req, res) {
-  // TODO: Implement Facebook OAuth flow
-  res.status(200).json({ success: true, platform: "Facebook" });
+  const clientId = process.env.FACEBOOK_CLIENT_ID;
+  const redirectUri = "http://localhost:3000/api/facebookCallback";
+
+  const authUrl = `https://www.facebook.com/v18.0/dialog/oauth?client_id=${clientId}&redirect_uri=${redirectUri}&scope=pages_manage_posts,pages_read_engagement`;
+
+  res.redirect(authUrl);
 }
