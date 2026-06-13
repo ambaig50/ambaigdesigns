8e69f3d3d2dcec45cc38aa11a4c393356b328183
ambaig50 <ambaig50@gmail.com>
2026-06-13 20:37:38 +0500

    Delete components/templates/Food/Fruit basket.js

--- components/templates/Food/Fruit basket.js
+++
@@ -1,9 +0,0 @@
-export default function FruitBasket({ title, description }) {
-  return (
-    <div style={{ background: "#f1f8e9", width: "600px", height: "900px", padding: "30px" }}>
-      <h1 style={{ fontSize: "28px", fontWeight: "bold", color: "#2e7d32" }}>{title}</h1>
-      <p style={{ fontSize: "18px", marginTop: "15px" }}>{description}</p>
-      <div style={{ marginTop: "40px", fontSize: "60px" }}>🍎🍌🍇</div>
-    </div>
-  );
-}
