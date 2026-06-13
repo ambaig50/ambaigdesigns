fd07610a166f80ea6df9900916aa8e6b8686ec94
ambaig50 <ambaig50@gmail.com>
2026-06-13 08:00:20 +0500

    Change Facebook access token to Meta access token

--- pages/api/postToFacebook.js
+++ pages/api/postToFacebook.js
@@ -1,7 +1,7 @@
 // pages/api/postToFacebook.js
 export default async function handler(req, res) {
   const { caption } = req.body;
-  const accessToken = process.env.FACEBOOK_ACCESS_TOKEN;
+  const accessToken = process.env.META_ACCESS_TOKEN;
 
   const response = await fetch(`https://graph.facebook.com/v18.0/{page_id}/feed`, {
     method: "POST",
