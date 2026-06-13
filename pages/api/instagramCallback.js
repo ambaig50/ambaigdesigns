472ff6bd69b56c97e42ca705515c9f7bff06e9e4
ambaig50 <ambaig50@gmail.com>
2026-06-13 07:56:14 +0500

    Update Instagram API client credentials to META

--- pages/api/instagramCallback.js
+++ pages/api/instagramCallback.js
@@ -1,8 +1,8 @@
 // pages/api/instagramCallback.js
 export default async function handler(req, res) {
   const { code } = req.query;
-  const clientId = process.env.INSTAGRAM_CLIENT_ID;
-  const clientSecret = process.env.INSTAGRAM_CLIENT_SECRET;
+  const clientId = process.env.META_CLIENT_ID;
+  const clientSecret = process.env.META_CLIENT_SECRET;
   const redirectUri = "http://localhost:3000/api/instagramCallback";
 
   const tokenRes = await fetch(`https://graph.instagram.com/v18.0/oauth/access_token?client_id=${clientId}&redirect_uri=${redirectUri}&client_secret=${clientSecret}&code=${code}`);
