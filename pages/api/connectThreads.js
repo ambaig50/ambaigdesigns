8d59d4083b739d583091bbe47230b5c1c99cc973
ambaig50 <ambaig50@gmail.com>
2026-06-20 07:10:55 +0500

    Delete connectThreads.js

--- pages/api/connectThreads.js
+++
@@ -1,9 +0,0 @@
-// pages/api/connectThreads.js
-export default async function handler(req, res) {
-  const clientId = process.env.META_CLIENT_ID;
-  const redirectUri = "http://localhost:3000/api/threadsCallback";
-
-  const authUrl = `https://www.threads.com/v18.0/dialog/oauth?client_id=${clientId}&redirect_uri=${redirectUri}&scope=pages_manage_posts,pages_read_engagement`;
-
-  res.redirect(authUrl);
-}
