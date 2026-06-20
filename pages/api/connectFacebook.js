ad1266ceb213ef233fc13086cb7dbe8c8023359d
ambaig50 <ambaig50@gmail.com>
2026-06-20 07:10:19 +0500

    Delete connectFacebook.js

--- pages/api/connectFacebook.js
+++
@@ -1,9 +0,0 @@
-// pages/api/connectFacebook.js
-export default async function handler(req, res) {
-  const clientId = process.env.META_CLIENT_ID;
-  const redirectUri = "http://localhost:3000/api/facebookCallback";
-
-  const authUrl = `https://www.facebook.com/v18.0/dialog/oauth?client_id=${clientId}&redirect_uri=${redirectUri}&scope=pages_manage_posts,pages_read_engagement`;
-
-  res.redirect(authUrl);
-}
