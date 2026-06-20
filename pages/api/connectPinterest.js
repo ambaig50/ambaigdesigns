154c7a3d18e5306742dc11eb2569dd354a443932
ambaig50 <ambaig50@gmail.com>
2026-06-20 07:10:43 +0500

    Delete connectPinterest.js

--- pages/api/connectPinterest.js
+++
@@ -1,9 +0,0 @@
-// pages/api/connectPinterest.js
-export default async function handler(req, res) {
-  const clientId = process.env.PINTEREST_CLIENT_ID;
-  const redirectUri = "http://localhost:3000/api/pinterestCallback";
-
-  const authUrl = `https://www.pinterest.com/v18.0/dialog/oauth?client_id=${clientId}&redirect_uri=${redirectUri}&scope=pages_manage_posts,pages_read_engagement`;
-
-  res.redirect(authUrl);
-}
