c0bc3cf963d232d0739ed28e6c257cb2cd1bdbfa
ambaig50 <ambaig50@gmail.com>
2026-07-08 11:59:12 +0500

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -4,8 +4,18 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
-        <meta name="theme-color" content="#0f0f13" />
+        {/* Favicon */}
+        <link rel="icon" href="/logo.png" type="image/png" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+
+        {/* PWA */}
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+        <meta name="apple-mobile-web-app-title" content="AmbaigDesigns" />
+        <meta name="application-name" content="AmbaigDesigns" />
+        <meta name="theme-color" content="#c084fc" />
         <meta charSet="utf-8" />
       </Head>
       <body>
